4bb582188d288c4f106f6c177274cb7104e68ba3
Brayan Alexander Salazar Reyes <bsalazarreyes88@gmail.com>
2024-05-29 17:21:08 -0500

    AlertaIncendios

---
+++ src/Components/Widgets/Mq_7.jsx
@@ -0,0 +1,11 @@
+import AnimatedGaugeWidget from "../Animations/AnimatedGaugeWidget"
+
+const Mq_7 = ({CO}) => {
+  return (
+    <AnimatedGaugeWidget 
+      CO = {CO}
+    />
+  )
+}
+
+export default Mq_7
